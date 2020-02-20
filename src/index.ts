291a0d85a68c73c27de23f0b0205ee16c4ffcedf
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2020-02-20 14:10:27 +0100

    Rename *zipWith into *zipMap and bump to v2.0.0
    BREAKING CHANGE

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
-import { flatMap, map } from 'rxjs/operators';
-import { ObservableInput, ObservedValueOf, of, OperatorFunction, zip } from 'rxjs';
+import { zipMap } from './operators/zipMap';
+import { flatZipMap } from './operators/flatZipMap';
 
-export function zipWith<T, R>(project: (v1: T) => R): OperatorFunction<T, [T, R]> {
-  return map(val => [val, project(val)]);
-}
-
-export function flatZipWith<T, O extends ObservableInput<any>>(
-  project: (value: T) => O,
-): OperatorFunction<T, [T, ObservedValueOf<O>]> {
-  return flatMap((val: T) => zip(of(val), project(val)));
-}
+export { zipMap, flatZipMap };
