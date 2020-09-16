537d515ea4ac6b71347c425aa0f26958953fd4d4
Mohammad-Ali A'RÂBI <aerabi@gmx.de>
2020-09-16 18:39:21 +0200

    Feat: Resurrect flatMap
    Just to get rid of the deprecation warning.
    
    Fixes #5

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { flatZipMap } from './operators/flatZipMap';
 import { projectToFormer, projectToLatter, projectTo } from './operators/projectTo';
 import { mapFormer, mapLatter } from './operators/mapCoordinate';
 import { flatMapFormer, flatMapLatter } from './operators/flatMapCoordinate';
+import { flatMap } from './operators/flatMap';
 
 export { zipMap, flatZipMap, projectToFormer, projectToLatter, projectTo };
 export { mapFormer, mapLatter, flatMapFormer, flatMapLatter };
+export { flatMap };
